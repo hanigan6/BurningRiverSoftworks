fb91744ede037a1b8cc7ca5b99586f31a29557ae
Neal Hanigan <Neal.Hanigan@taylor.com>
2021-05-30 15:01:40 -0400

    sample apps init

---
+++ Reactweb/react-web/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function BurningRiverWeb() {
+  return (
+    <div className="BurningRiverWeb">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default BurningRiverWeb;
